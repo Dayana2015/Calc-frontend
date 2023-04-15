ae42a84de7006fe16a9ebd70d2a2cad2e4cc8a00
Brillant-Minds-Grupo-5 <mosanz02@gmail.com>
2023-04-15 16:49:10 -0500

    v0.2

---
+++ programa.js
@@ -0,0 +1,33 @@
+
+//crear everlisten 1. tener una referencia al elemento del domo alq ue queremos agregarle el eventlistener
+const miBoton = document.getElementById("submit");
+
+miBoton.addEventListener(
+    "click",
+    async (event) => {
+
+        event.preventDefault();
+
+        console.log(event);
+        const numero_1 = parseFloat(document.getElementById("num1").value) ;
+        const numero_2 = parseFloat(document.getElementById("num2").value);
+        //crear peticion al backend con fetch
+        const respuesta= await fetch(
+            `http://localhost:3000/api/sumar`,
+            {
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify({numero_1, numero_2})
+            }
+
+        );
+        const dato= await respuesta.json();
+        const div_resultado=document.getElementById("resultado");
+        div_resultado.innerHTML=dato;
+
+
+    }
+
+);
